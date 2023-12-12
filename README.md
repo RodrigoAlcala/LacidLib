bf1066e89aa94b386b4741c3ca8d052043785fb5
Rodrigo N. Alcalá M <rodrigoalcala00@yahoo.com>
2023-12-12 13:16:55 -0300

    added setup and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install Guide:
+
+Make sure your present working directory is /path/to/mypythonlibrary
+
+Then:
+
+python setup.py bdist_wheel
+
+pip install /path/to/wheelfile.whl
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lacidlib',
+    packages=find_packages(include=['lacidlib']),
+    version='0.1.0',
+    description='General Dicom Processing library created by LACID - FUESMEN (Mendoza, Argentina)',
+    author='Rodrigo N. Alcalá M. & Daniel Fino V.'
+)
